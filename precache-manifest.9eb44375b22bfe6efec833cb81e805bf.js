d001bf69d64f36855f987601b73d841cd38f2155
Lu Jin <lillianjin67@gmail.com>
2019-03-11 23:47:34 -0500

    Updates

---
+++ precache-manifest.9eb44375b22bfe6efec833cb81e805bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2358a69a7a94285221b2",
+    "url": "/498mp2/static/js/runtime~main.2358a69a.js"
+  },
+  {
+    "revision": "9775eee1bf1683448f83",
+    "url": "/498mp2/static/js/main.9775eee1.chunk.js"
+  },
+  {
+    "revision": "a2d3406b29c3811a804b",
+    "url": "/498mp2/static/js/2.a2d3406b.chunk.js"
+  },
+  {
+    "revision": "9775eee1bf1683448f83",
+    "url": "/498mp2/static/css/main.1d0055c5.chunk.css"
+  },
+  {
+    "revision": "6256753db7ad3c2003bd9a88fec2cd3c",
+    "url": "/498mp2/index.html"
+  }
+];
