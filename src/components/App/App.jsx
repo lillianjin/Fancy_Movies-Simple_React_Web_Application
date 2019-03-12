2c5650f7fadb2e0662796f2c9669d350f1474993
Lu Jin <lillianjin67@gmail.com>
2019-03-12 00:17:06 -0500

    fifth submit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import { HashRouter as Router, Route, Switch} from 'react-router-dom'
+import {HashRouter as Router, Route, Switch} from 'react-router-dom'
 import './App.css';
+// import history from './history';
 
 // Include your new Components here
 import Home from '../Home/Home.jsx';
@@ -9,17 +10,16 @@ import Details from '../Details/Details.jsx';
 import Details0 from '../Details/Details0.jsx';
 
 class App extends Component {
-
   render() {
     return (
       <div id="root">
         <Router>
-          <Switch>
+          <div>
             <Route exact path="/" component={Home}/>
             <Route path="/list" component={List}/>
             <Route path="/details/:index" component={Details}/>
             <Route path="/details0/:index" component={Details0}/>
-            </Switch>
+          </div>
         </Router>
       </div>
     );
